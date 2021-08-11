00a344c2f88752efcd51efa9463c1684fff51444
baekseungyeop <tmdduq5498@gmail.com>
2021-08-11 10:37:35 +0900

    Constructor 예제 추가

---
+++ ConstructorPractice/src/kr/ac/daegu/bsy/Person.java
@@ -0,0 +1,18 @@
+package kr.ac.daegu.bsy;
+
+public class Person {
+    int age;
+    String name;
+    int height;
+    int weight;
+
+    public Person(){
+    }
+    public Person(int age){
+        this.age = age;
+    }
+    public Person(String name){
+        this.name = name;
+    }
+}
+
