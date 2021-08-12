ace39c07e6038ce464d6d637754b13ba20c07757
baekseungyeop <tmdduq5498@gmail.com>
2021-08-12 17:00:31 +0900

    Annotation 예제 추가

---
+++ AnnotationPractice/src/kr/ac/daegu/bsy/AnnotationExam02.java
@@ -0,0 +1,20 @@
+package kr.ac.daegu.bsy;
+
+public class AnnotationExam02 {
+    @InsertStringData(data =  "MHLab")
+    private String myData;
+
+    @InsertStringData
+    private String defaultData;
+
+    public AnnotationExam02(){
+        myData = "No data";
+        defaultData = "No data";
+    }
+    public String getMyData() {
+        return myData;
+    }
+    public String getDefaultData() {
+        return  defaultData;
+    }
+}
