b60e49ffadd15f93decae657f87dd0ca58fbb2c2
baekseungyeop <tmdduq5498@gmail.com>
2021-08-13 15:55:37 +0900

    JavaCorePractice 개발환경 설정 (모듈 연결)

--- ExtendsAbstractPractice/src/kr/ac/daegu/bsy/PizzaTestDrive.java
+++ ExtendsAbstractPractice/src/kr/ac/daegu/bsy/PizzaTestDrive.java
@@ -3,12 +3,12 @@ package kr.ac.daegu.bsy;
 public class PizzaTestDrive {
 
     public static void main(String[] args) {
-        PizzaStore nyStore = new NYPizzStore();
-        PizzaStore ChicagoStore = new ChicagoPizzaStore();
-        Pizza nyStylePizza = nyStore.orderPizze("cheese");
-        System.out.println(nyStylePizza.getName());
-        System.out.println("-------------------------------------------------");
-        Pizza chicagoStylePizza = chicagoStore.orderPizza("cheese");
-        System.out.println(chicagoStylePizza.getName());
+//        PizzaStore nyStore = new NYPizzStore();
+//        PizzaStore ChicagoStore = new ChicagoPizzaStore();
+//        Pizza nyStylePizza = nyStore.orderPizze("cheese");
+//        System.out.println(nyStylePizza.getName());
+//        System.out.println("-------------------------------------------------");
+//        Pizza chicagoStylePizza = chicagoStore.orderPizza("cheese");
+//        System.out.println(chicagoStylePizza.getName());
     }
 }
--- FileExample4/src/kr/ac/daegu/bsy/FileOutputStreamTest.java
+++ FileExample4/src/kr/ac/daegu/bsy/FileOutputStreamTest.java
@@ -12,7 +12,7 @@ public class FileOutputStreamTest {
         // java 코드에서 생성한 문자열 데이터를 FileOutputStream 이용하여 fileOutput.txt에 쓰기.
         FileOutputStream fo = null;
         String msg = "FileOutputStream Test 안녕하세요";
-        byte[] byteArray = msg.getBytes(); // msg.getBytes(StandardCharsets.UTF_8)안전하게 UTF=8인코딩 지정하여 다국어(한글) 지정 할 수 있도록
+        byte[] byteArray = msg.getBytes(StandardCharsets.UTF_8); // msg.getBytes(StandardCharsets.UTF_8)안전하게 UTF=8인코딩 지정하여 다국어(한글) 지정 할 수 있도록
 
         try{
             // 상대경로를 지정할때 어떻게 테스트 해가면서 쓸지? : ./ 찍어서 실제 동작이 어떻게 되는지 확인하고 작성.
