89240f8a7b587b927c264cc7115d6e52b3523158
baekseungyeop <tmdduq5498@gmail.com>
2021-08-13 16:43:46 +0900

    JavaCorePractice 개발환경 설정 (모듈 연결) JDK16으로 되어있어 에러가 나 1.8로 수정

--- FileExample7/src/kr/ac/daegu/bsy/Main.java
+++ FileExample7/src/kr/ac/daegu/bsy/Main.java
@@ -3,6 +3,7 @@ package kr.ac.daegu.bsy;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -18,7 +19,19 @@ public class Main {
             buW.write('B');
             buW.write('C');
             buW.write('D');
-            //buW
+
+            Scanner sc = new Scanner(System.in);
+            String input = null;
+            do {
+                System.out.println("입력하시오.");
+                input = sc.next();
+                if(input.equals("end")){
+                    System.exit(0); // "end" 입력 시 강제종료
+                }
+            } while(!input.equals("Q")); // "Q" 입력 받을 때 까지 계속 반복
+
+            // 데이터가 버퍼 사이즈만큼 전부 채워 지길 기다렸다가 flush 하는 경우 (비움)
+            buW.flush();
         }
         catch (IOException io){
             io.printStackTrace();
